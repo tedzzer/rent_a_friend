7a47e53c3eac6878ef356b8b5330770a34dd448c
Maxime Japsenne <japsenne@gmail.com>
2022-05-24 15:50:43 +0200

    saving

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,6 @@
 class FriendsController < ApplicationController
   def index
-    @friends = Friend.all
+    @friends = policy_scope(Friend).order(created_at: :desc)
   end
 
   def show
@@ -9,12 +9,14 @@ class FriendsController < ApplicationController
 
   def new
     @friend = Friend.new
+    authorize @friend
   end
 
   def create
     @friend = Friend.new(friend_params)
     @friend.save
-    redirect_to friends_path
+    authorize @friend
+    redirect_to friend_path(@friend)
   end
 
   def edit
@@ -24,12 +26,14 @@ class FriendsController < ApplicationController
   def update
     @friend = Friend.find(params[:id])
     @friend.update(friend_params)
+    authorize @friend
     redirect_to friends_path
   end
 
   def destroy
     @friend = Friend.find(params[:id])
     @friend.destroy
+    authorize @friend
     redirect_to friends_path
   end
 
