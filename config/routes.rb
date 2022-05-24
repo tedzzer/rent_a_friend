0a92df37bd62e65ae3b804b803a6ced15bb710c5
Maxime Japsenne <japsenne@gmail.com>
2022-05-24 12:27:32 +0200

    saving

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/friends',     to: 'friends#index'
+  get 'friends/new',  to: 'friends#new', as: :new_friend # The `new` route needs to be *before* `show` route.
+
+  get '/friends/:id', to: 'friends#show', as: :friend
+  post 'friends',     to: 'friends#create'
+
+  get 'friends/:id/edit', to: 'friends#edit', as: :edit_friend
+  patch "friends/:id",    to: "friends#update"
+
+  delete "friends/:id", to: "friends#destroy", as: :dell
 end
--- db/migrate/20220524092534_create_friends.rb
+++ db/migrate/20220524092534_create_friends.rb
@@ -1,6 +1,9 @@
 class CreateFriends < ActiveRecord::Migration[6.1]
   def change
     create_table :friends do |t|
+      t.text :description
+      t.string :location
+      t.string :price
 
       t.timestamps
     end
