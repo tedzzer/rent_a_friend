25d19c024fe625672ce6eb57cd9eca6ce5f1fef8
Maxime Japsenne <japsenne@gmail.com>
2022-05-24 12:34:51 +0200

    updating database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_23_154546) do
+ActiveRecord::Schema.define(version: 2022_05_24_092534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "friends", force: :cascade do |t|
+    t.text "description"
+    t.string "location"
+    t.string "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
