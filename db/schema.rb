e4dbcc66c0e47986684822c1440fe3299d537572
Ted Andre <elleted@hotmail.com>
2022-05-25 14:35:59 +0200

    navbar space fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2022_05_24_132839) do
-=======
-
-ActiveRecord::Schema.define(version: 2022_05_24_132839) do
-
-
-
->>>>>>> 685bc77ddea2b9fb3c4df3147443e4e7dbdf21d0
+ActiveRecord::Schema.define(version: 2022_05_24_133748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,17 +49,6 @@ ActiveRecord::Schema.define(version: 2022_05_24_132839) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
-    t.text "comment"
-    t.bigint "user_id", null: false
-    t.bigint "friend_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["friend_id"], name: "index_reviews_on_friend_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -84,15 +65,9 @@ ActiveRecord::Schema.define(version: 2022_05_24_132839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "reviews", "friends"
-  add_foreign_key "reviews", "users"
-
-
+  add_foreign_key "friends", "users"
   add_foreign_key "reservations", "friends"
   add_foreign_key "reservations", "users"
-
-  add_foreign_key "friends", "users"
-
-
+  add_foreign_key "reviews", "friends"
+  add_foreign_key "reviews", "users"
 end
