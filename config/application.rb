b86b7af7196351505218a882e0f4467d82e19eb4
Ted Andre <elleted@hotmail.com>
2022-05-25 16:33:57 +0200

    saving after comment applicable.rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RentAFriend
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    # config.load_defaults 6.1
     config.autoload = :classic
     # Configuration for the application, engines, and railties goes here.
     #
